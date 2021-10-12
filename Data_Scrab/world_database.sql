7e16390cd6d235aab9da1d7bcf792e22685c1fd8
NikunjPatel852 <84136692+NikunjPatel852@users.noreply.github.com>
2021-10-12 22:05:38 +0800

    add new flask code

--- Data_Scrab/world_database.sql
+++ Data_Scrab/world_database.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS "worlddata";
 CREATE TABLE "worlddata" (
+	"index" int NOT NULL PRIMARY KEY,
     "rep_country_code" int NOT NULL,
     "rep_countries" varchar,
     "par_country_code" int,
--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-import numpy as np
+import os
 import pandas as pd
-import datetime as dt
+import json
+import numpy as np 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+from sqlalchemy.orm import Session,sessionmaker
+from sqlalchemy import create_engine, distinct
+from sqlalchemy.sql import func
 
-engine = create_engine("sqlite:///Data_Scrab/trade_world.db")
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import ServerName, UserName, Password, DataBase
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-print(Base.classes.keys())
-
-#trade = Base.classes.worlddata
-
-session = Session(engine)
-
-#weather app
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','') or f"postgres://{UserName}:{Password}@localhost:5432/{DataBase}"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+worlddata = Base.classes.worlddata
+
+# @app.route("/")
+# def index():
+#     """Return the homepage."""
+#     return render_template("index.html")
+
+@app.route("/api/v1.0/names")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(db.engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(worlddata.year).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_year = list(np.ravel(results))
+    #print(all_year)
+    return jsonify(all_year)
+
+if __name__ == "__main__":
+    app.debug = False
+    app.run()
