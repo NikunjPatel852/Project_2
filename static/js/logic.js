7a3eb3d9ecfa8b9d17cc21b7bf8650f1dbfd29a3
Shaun Mwambada <shaun.mwambada@gmail.com>
2021-10-18 20:01:04 +0800

    working list and unusable find button on home page

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,106 +7,119 @@
 //     });
 
 
-
 var url = "http://127.0.0.1:5000/countrieslist";
-d3.json(url).then(function(data) {
-    var countryList = data;
-    console.log(countryList);
-    var country_dropdown = document.getElementById("country");
-    countryList.forEach(country => {
-        country_dropdown.options.add(new Option(country,country))
+d3.json(url).then(function (data) {
+        var countryList = data;
+        console.log(countryList);
+        var country_dropdown = document.getElementById("country");
+        countryList.forEach(country => {
+            country_dropdown.options.add(new Option(country, country));
+        });
+        country_dropdown.onchange = event => {
+            console.log(event.target.value);
+        };
     });
-    country_dropdown.onchange = function (event){
-        console.log(event.target.value)
-    }
-});
-//Initial connection to api. 
-var countryUrl='http://127.0.0.1:5000/countrieslist';
-d3.json(countryUrl).then(function(data){
-    var countryName = [data];
-    console.log(countryName);
-}); 
-var yearurl='http://127.0.0.1:5000/yearslist';
-d3.json(yearurl).then(function(data){
-    var yearslist = [data];
-    console.log(yearslist)
-}); 
 
-// var exporturl='http://127.0.0.1:5000/exportdata2011';
-// d3.json(exporturl).then(function(data){
-//     var exportHomeCountry = [data.rep_countries];
-//     console.log(exportHomeCountry);
-//     var exportPartCountry = [data.par_countries];
-//     console.log(exportPartCountry);
-//     var exportItem = [data.item];
-//     console.log(exportItem);
-//     var exportValue = [data.value];
-//     console.log(exportValue);
-//     buildTable(exportHomeCountry, exportPartCountry, exportItem, exportValue)
+
+// var url = "http://127.0.0.1:5000/yearlist";
+// d3.json(url).then(function(data) {
+//     var yearList = data;
+//     console.log(yearList);
+//     var year_dropdown = document.getElementById("year");
+//     countryList.forEach(country => {
+//         country_dropdown.options.add(new Option(year,year))
+//     });
+//     country_dropdown.onchange = function (event){
+//         console.log(event.target.value)
+//     }
+// });
+
+// //Initial connection to api. 
+// var countryUrl='http://127.0.0.1:5000/countrieslist';
+// d3.json(countryUrl).then(function(data){
+//     var countryName = [data];
+//     console.log(countryName);
+// }); 
+// var yearurl='http://127.0.0.1:5000/yearslist';
+// d3.json(yearurl).then(function(data){
+//     var yearslist = [data];
+//     console.log(yearslist)
 // }); 
 
-var importUrl='http://127.0.0.1:5000/importdata2011';
-d3.json(importUrl).then(function(data){
-    var importdata2011 = [data];
-    // console.log(importdata2011);
-    const obj = Object.assign({}, importdata2011);
-    // console.log(obj);
+// // var exporturl='http://127.0.0.1:5000/exportdata2011';
+// // d3.json(exporturl).then(function(data){
+// //     var exportHomeCountry = [data.rep_countries];
+// //     console.log(exportHomeCountry);
+// //     var exportPartCountry = [data.par_countries];
+// //     console.log(exportPartCountry);
+// //     var exportItem = [data.item];
+// //     console.log(exportItem);
+// //     var exportValue = [data.value];
+// //     console.log(exportValue);
+// //     buildTable(exportHomeCountry, exportPartCountry, exportItem, exportValue)
+// // }); 
 
-  var importHomeCountry = obj[0].rep_countries;
-  // console.log(importHomeCountry);
-  var importPartCountry = obj[0].par_countries;
-  // console.log(importPartCountry);
-  var importItem = obj[0].item;
-  // console.log(importItem);
-  var importValue = obj[0].value;
-  console.log(importValue);
-  buildTable(importHomeCountry, importPartCountry,importItem, importValue);
+// var importUrl='http://127.0.0.1:5000/importdata2011';
+// d3.json(importUrl).then(function(data){
+//     var importdata2011 = [data];
+//     // console.log(importdata2011);
+//     const obj = Object.assign({}, importdata2011);
+//     // console.log(obj);
 
-  function buildTable(importHomeCountry, importPartCountry,importItem, importValue) {
-    var table = d3.select("#ufo-table");
-    var tbody = table.select("tbody");
-    var trow;
-    for (var i = 0; i < 12; i++) {
-      trow = tbody.append("tr");
-      trow.append("td").text(importHomeCountry[i]);
-      trow.append("td").text(importPartCountry[i]);
-      trow.append("td").text(importItem[i]);
-      trow.append("td").text(importValue[i]);
-    }
-  }
+//   var importHomeCountry = obj[0].rep_countries;
+//   // console.log(importHomeCountry);
+//   var importPartCountry = obj[0].par_countries;
+//   // console.log(importPartCountry);
+//   var importItem = obj[0].item;
+//   // console.log(importItem);
+//   var importValue = obj[0].value;
+//   console.log(importValue);
+//   buildTable(importHomeCountry, importPartCountry,importItem, importValue);
 
-  function buildGraph(importHomeCountry, importPartCountry,importItem, importValue) {
-    var trace1 = {
-      x: obj.map(row => row.value),
-      y: obj.map(row => row.item),
-      text: obj.map(row => row.),
-      name: "Greek",
-      type: "bar",
-      orientation: "h"
-    };
+//   function buildTable(importHomeCountry, importPartCountry,importItem, importValue) {
+//     var table = d3.select("#ufo-table");
+//     var tbody = table.select("tbody");
+//     var trow;
+//     for (var i = 0; i < 12; i++) {
+//       trow = tbody.append("tr");
+//       trow.append("td").text(importHomeCountry[i]);
+//       trow.append("td").text(importPartCountry[i]);
+//       trow.append("td").text(importItem[i]);
+//       trow.append("td").text(importValue[i]);
+//     }
+//   }
+
+//   function buildGraph(importHomeCountry, importPartCountry,importItem, importValue) {
+//     var trace1 = {
+//       x: obj.map(row => row.value),
+//       y: obj.map(row => row.item),
+//       text: obj.map(row => row.),
+//       name: "Greek",
+//       type: "bar",
+//       orientation: "h"
+// //     }};
   
-    // data
-    var chartData = [trace1];
+//     // data
+//     var chartData = [trace1];
   
-    // Apply the group bar mode to the layout
-    var layout = {
-      title: "Greek gods search results",
-      margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-      }
-    };
+//     // Apply the group bar mode to the layout
+//     var layout = {
+//       title: "Greek gods search results",
+//       margin: {
+//         l: 100,
+//         r: 100,
+//         t: 100,
+//         b: 100
+//       }
+//     };
   
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("plot", chartData, layout);
+//     // Render the plot to the div tag with id "plot"
+//     Plotly.newPlot("plot", chartData, layout);
 
 
-  }
+//   }
 
-  
-}); 
+// ,); 
 
 
 
