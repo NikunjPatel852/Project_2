82febaee6505886b681abc4dfa20e4ceed48fab8
NikunjPatel852 <84136692+NikunjPatel852@users.noreply.github.com>
2021-10-19 15:38:29 +0800

    add upadats to html and app

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def countrieslist():
 
 
 # for the country
-@app.route("/<country>/stats")
-def countryStats(country):
+@app.route("/<country>/exporttotalvalue")
+def exportcountryStats(country):
     print(country)
     export_rs = db.session.execute(""" 
     select sum(value) as value2, year
@@ -70,21 +70,40 @@ def countryStats(country):
         })
     return jsonify(export_stats)
 
-@app.route("/importvaluedata")
-def import2011data():
+@app.route("/<country>/importtotalvalue")
+def importcountryStats(country):
+    print(country)
+    import_rs = db.session.execute(""" 
+    select sum(value) as value2, year
+    from worlddata 
+    where rep_countries = :country and element = 'Import Value'
+    group by year
+    """, {"country":country})
+    import_stats = []
+    for stat in import_rs:
+        import_stats.append({
+            "value": stat["value2"],
+            "year": stat["year"]
+        })
+    return jsonify(import_stats)
+
+@app.route("/<country>/importvaluedata")
+def importvaldata(country):
+    print(country)
     # Create our session (link) from Python to the DB
-    session = Session(db.engine)
+    # session = Session(db.engine)
 
     """Return a list of all country names"""
-    # Query all passengers
-    import_data = session.query(
-        worlddata.rep_countries, 
-        worlddata.par_countries, 
-        worlddata.year, 
-        worlddata.element, 
-        worlddata.value, 
-        worlddata.item).filter_by(element= 'Import Value').limit(10000).all()
-
+    # Query all Import Values 
+    # import_data = session.query(
+    #     worlddata.rep_countries, 
+    #     worlddata.par_countries, 
+    #     worlddata.year, 
+    #     worlddata.element, 
+    #     worlddata.value, 
+    #     worlddata.item).filter_by(rep_countries= ':country',element= 'Import Value', {"country":country}).limit(10000).all()
+    
+    import_data = db.session.execute("""select rep_countries,par_countries,year,element,value,item from worlddata where element = 'Import Value' and rep_countries = :country""", {"country":country})
     importV= pd.DataFrame(import_data, columns=[
         'rep_countries', 
         'par_countries', 
@@ -109,6 +128,54 @@ def import2011data():
     importV3 = importV2.sort_values('value',ascending=False).reset_index()
     #importV3.head()
     importV4 = importV3.drop(columns=['element','index'])
+    importV4 = importV4[importV4.value !=0]
+    #importV4.head(50)
+    importV5 = importV4.to_json(orient='records')
+    
+    return importV5
+
+@app.route("/australia/importvaluedata")
+def imvaluedata():
+    #Create our session (link) from Python to the DB
+
+    """Return a list of all country names"""
+    #Query all Import Values 
+    # element = ['Import Value', 'Import Quantity']
+    # import_data = session.query(
+    #     worlddata.rep_countries, 
+    #     worlddata.par_countries, 
+    #     worlddata.year, 
+    #     worlddata.element,
+    #     worlddata.unit, 
+    #     worlddata.value, 
+    #     worlddata.item).filter_by(rep_countries= 'Australia',element='Import Value').all()
+    impor_data = db.session.execute("""select rep_countries,par_countries,year,element,unit,value,item from worlddata where rep_countries = 'Australia' and (element = 'Import Value' or element = 'Import Quantity')""")
+    importV= pd.DataFrame(impor_data, columns=[
+        'rep_countries', 
+        'par_countries', 
+        'year',
+        'element',
+        'unit', 
+        'value', 
+        'item'])
+    
+    importV.sort_values('value',ascending=False).groupby(['par_countries', 'item'])['value'].sum()
+    importV.columns.get_level_values(0)
+    importV.columns.to_flat_index()
+    importV.columns = ['_'.join(x) for x in importV.columns.to_flat_index()]
+    
+    importV2 =importV.rename(columns= {
+        'r_e_p___c_o_u_n_t_r_i_e_s': 'rep_countries',
+        'p_a_r___c_o_u_n_t_r_i_e_s': 'par_countries',
+        'y_e_a_r': 'year',
+        'e_l_e_m_e_n_t': 'element',
+        'u_n_i_t': 'unit',
+        'v_a_l_u_e': 'value',
+        'i_t_e_m': 'item'})
+    
+    importV3 = importV2.sort_values('value',ascending=False).reset_index()
+    #importV3.head()
+    importV4 = importV3.drop(columns=['index'])
     importV4 = importV4[importV4.value !=0]
     #importV4.head(50)
     importV5 = importV4.to_json(orient='records')
@@ -159,31 +226,32 @@ def importquantdata():
     
     return importV5
 
-@app.route("/exportvaluedata")
+@app.route("/australia/exportvaluedata")
 def exportvaldata():
     # Create our session (link) from Python to the DB
     session = Session(db.engine)
 
     """Return a list of all country names"""
     # Query all passengers
-    exportv = session.query(
-        worlddata.rep_countries, 
-        worlddata.par_countries, 
-        worlddata.year, 
-        worlddata.element, 
-        worlddata.value, 
-        worlddata.item).filter_by(
-            element= 'Export Value', year = 2012).limit(1000).all()
-
+    # exportv = session.query(
+    #     worlddata.rep_countries, 
+    #     worlddata.par_countries, 
+    #     worlddata.year, 
+    #     worlddata.element, 
+    #     worlddata.value, 
+    #     worlddata.item).filter_by(
+    #         element= 'Export Value', year = 2012).limit(1000).all()
+    exportv = db.session.execute("""select rep_countries,par_countries,year,element,unit,value,item from worlddata where rep_countries = 'Australia' and (element = 'Export Value' or element = 'Export Quantity')""")
     exportV= pd.DataFrame(exportv, columns=[
         'rep_countries', 
         'par_countries', 
         'year',
-        'element', 
+        'element',
+        'unit', 
         'value', 
         'item'])
     
-    exportV.sort_values('value',ascending=False).groupby(['rep_countries', 'item'])['value'].sum()
+    exportV.sort_values('value',ascending=False).groupby(['par_countries', 'item'])['value'].sum()
     exportV.columns.get_level_values(0)
     exportV.columns.to_flat_index()
     exportV.columns = ['_'.join(x) for x in exportV.columns.to_flat_index()]
@@ -192,12 +260,13 @@ def exportvaldata():
         'p_a_r___c_o_u_n_t_r_i_e_s': 'par_countries',
         'y_e_a_r': 'year',
         'e_l_e_m_e_n_t': 'element',
+        'u_n_i_t': 'unit',
         'v_a_l_u_e': 'value',
         'i_t_e_m': 'item'})
     
     exportV3 = exportV2.sort_values('value',ascending=False).reset_index()
     #exportV3.head()
-    exportV4 = exportV3.drop(columns=['element','index', 'year'])
+    exportV4 = exportV3.drop(columns=['index'])
     #exportV4.head(50)
     exportV5 = exportV4.to_json(orient='records')
     
--- static/js/logic.js
+++ static/js/logic.js
@@ -11,44 +11,76 @@ d3.json(url).then(function (data) {
     };
 });
 
-var importUrl = 'http://127.0.0.1:5000/importvaluedata';
-d3.json(importUrl).then(function (data){
-  console.log(data);
+var ausUrl = 'http://127.0.0.1:5000/australia/importvaluedata'
+d3.json(ausUrl).then(function (data){
+    console.log(data);
 
-  var table = new Tabulator("#example-table1", {
-      data:data,
-      height:"311px",
-      layout:"fitColumns",
-      columns:[ 
-          {title:"Partner Country", field:"par_countries", width:150, headerFilter:"input"},   
-          {title:"Year", field:"year", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
-          {title:"Iteam", field:"item", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
-          {title:"Value", field:"value", hozAlign:"center", sorter:"value",  headerFilter:"input"},
-      ],
-  });
+var table = new Tabulator("#example-table1", {
+    data:data,
+    height:"311px",
+    layout:"fitColumns",
+    columns:[ 
+        {title:"Partner Country", field:"par_countries", width:150, headerFilter:"input"},
+        {title:"Element", field:"element", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},   
+        {title:"Year", field:"year", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Iteam", field:"item", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Unit", field:"unit", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Value", field:"value", hozAlign:"center", sorter:"number", bottomCalc:"sum", bottomCalcParams:{precision:3}},
+    ],
+});
+
+
+
 
 });
 
-var exportUrl = 'http://127.0.0.1:5000/exportvaluedata';
+var exportUrl = 'http://127.0.0.1:5000/australia/exportvaluedata';
 d3.json(exportUrl).then(function (data){
-  console.log(data);
+console.log(data);
 
-  var table = new Tabulator("#example-table2", {
-      data:data,
-      height:"311px",
-      layout:"fitColumns",
-      columns:[ 
-          {title:"Partner Country", field:"par_countries", width:150, headerFilter:"input"},   
-          {title:"Year", field:"year", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
-          {title:"Iteam", field:"item", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
-          {title:"Value", field:"value", hozAlign:"center", sorter:"value",  headerFilter:"input"},
-      ],
-  });
+var table = new Tabulator("#example-table2", {
+    data:data,
+    height:"311px",
+    layout:"fitColumns",
+    columns:[ 
+        {title:"Partner Country", field:"par_countries", width:150, headerFilter:"input"},
+        {title:"Element", field:"element", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},   
+        {title:"Year", field:"year", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Iteam", field:"item", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Unit", field:"unit", editor:"input", headerFilter:"select", headerFilterParams:{values:true}},
+        {title:"Value", field:"value", hozAlign:"center", sorter:"number", bottomCalc:"sum", bottomCalcParams:{precision:3}},
+    ],
+});
 
+//trigger download of data.csv file
+document.getElementById("download-csv").addEventListener("click", function(){
+    table.download("csv", "data.csv");
+});
+
+//trigger download of data.json file
+document.getElementById("download-json").addEventListener("click", function(){
+    table.download("json", "data.json");
+});
+
+//trigger download of data.xlsx file
+document.getElementById("download-xlsx").addEventListener("click", function(){
+    table.download("xlsx", "data.xlsx", {sheetName:"My Data"});
+});
+
+//trigger download of data.pdf file
+document.getElementById("download-pdf").addEventListener("click", function(){
+    table.download("pdf", "data.pdf", {
+        orientation:"portrait", //set page orientation to portrait
+        title:"Example Report", //add title to report
+    });
+});
+
+//trigger download of data.html file
+document.getElementById("download-html").addEventListener("click", function(){
+    table.download("html", "data.html", {style:true});
 });
 
 
-
-
+});
 
 
